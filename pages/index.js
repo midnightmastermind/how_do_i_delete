51ba986e8165ae7f74d1294b926940dd4751c760
Josh Pomerenke <jtpomerenke@gmail.com>
2021-12-04 13:24:13 -0600

    added new logo, fixed some css, and added in schedule

--- pages/index.js
+++ pages/index.js
@@ -13,18 +13,56 @@ export default function Home() {
             <div className="App-welcomescreen">
               <div className="Landing-title">
                  <div className="App-logo" alt="logo" />
-                  <h2>Saddie with a Faddie</h2>
                  <div className="titles">
-                  <div>Only Fans Model</div>
+                  <div className="onlyfans-icon" />
+                  <div className="onlyfans-title">Only Fans Model</div>
                  </div>
               </div>
               <div className="Landing-contact">
-                <div><a href="mailto:alexis@saddiewithafaddie.com"><FontAwesomeIcon icon={faEnvelope} />alexis@saddiewithafaddie.com</a></div>
+                <div><a href="mailto:alexis@saddiewithafaddie.com"><FontAwesomeIcon className="icon" icon={faEnvelope} />alexis@saddiewithafaddie.com</a></div>
               </div>
-              <div className="Landing-links">
-                <a className="Landing-link" color="inherit" href="http://onlyfans.com/saddiewithafaddie"><div className="onlyfans-icon"/>Only Fans</a>
-                <a className="Landing-link" color="inherit" href="https://www.instagram.com/saddiewith.afaddie"><FontAwesomeIcon className="icon" icon={faInstagram}/>Instagram</a>
-                <a className="Landing-link" color="inherit" href="https://www.twitter.com/saddywithafaddy"><FontAwesomeIcon className="icon" icon={faTwitter}/>Twitter</a>
+              <div className="Landing-container">
+                <div className="Landing-links">
+                  <a className="Landing-link" color="inherit" href="http://onlyfans.com/saddiewithafaddie"><div className="onlyfans-icon" />Only Fans</a>
+                  <a className="Landing-link" color="inherit" href="https://www.instagram.com/saddiewith.afaddie"><FontAwesomeIcon className="icon" icon={faInstagram}/>Instagram</a>
+                  <a className="Landing-link" color="inherit" href="https://www.twitter.com/saddywithafaddy"><FontAwesomeIcon className="icon" icon={faTwitter}/>Twitter</a>
+                </div>
+                <div className="Landing-schedule">
+                  <div className="schedule-title">Schedule</div>
+                  <div className="schedule">
+                    <table>
+                      <tr>
+                          <td>Monday</td>
+                          <td>Mini Set</td>
+                      </tr>
+                      <tr>
+                          <td>Tuesday</td>
+                          <td>Alt Content</td>
+                      </tr>
+                      <tr>
+                          <td>Wednesday</td>
+                          <td>Full Set</td>
+                      </tr>
+                      <tr>
+                          <td>Thursday</td>
+                          <td>Teaser + PPV</td>
+                      </tr>
+
+                      <tr>
+                          <td>Friday</td>
+                          <td>Full Set</td>
+                      </tr>
+                      <tr>
+                          <td>Saturday</td>
+                          <td>Teaser + PPV</td>
+                      </tr>
+                      <tr>
+                          <td>Sunday</td>
+                          <td>No Posts</td>
+                      </tr>
+                    </table>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
