ec2d7a3e5ad82649d12ca38e4974b96de5c3de5b
Josh Pomerenke <jtpomerenke@gmail.com>
2022-01-06 14:12:12 -0600

    added mobile css changes

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
 import '../styles/globals.css'
-
+import Head from "next/head";
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="keywords" content="saddie, faddie" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no" />
+      </Head>
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
 export default MyApp
